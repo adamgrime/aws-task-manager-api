21e8cb5af68b8145bda1be7d871926d050cd344c
Adam Grime <adamxgrime@gmail.com>
2025-08-28 16:44:23 +0100

    Update Function method to handle logic

--- Function.cs
+++ Function.cs
@@ -1,21 +1,95 @@
+using System.Net;
+using System.Text.Json;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using TaskManager.Core;
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace TaskManager.Api;
+namespace TaskManager.API;
 
 public class Function
 {
-    
+    private readonly IDynamoDBContext _context;
+
     /// <summary>
-    /// A simple function that takes a string and does a ToUpper
+    /// This constructor is used by Lambda to instantiate the function.
     /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public Function()
     {
-        return input.ToUpper();
+        IAmazonDynamoDB client = new AmazonDynamoDBClient();
+        _context = new DynamoDBContext(client);
+    }
+
+    /// <summary>
+    /// This constructor is used by unit tests.
+    /// </summary>
+    public Function(IDynamoDBContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        context.Logger.LogLine($"Received API Gateway request with HTTP Method: {request.HttpMethod}");
+
+        return request.HttpMethod switch
+        {
+            "GET" => await ListTasks(),
+            "POST" => await CreateTask(request),
+            _ => new APIGatewayProxyResponse { StatusCode = (int)HttpStatusCode.MethodNotAllowed, Body = "{\"message\":\"Method not allowed.\"}" },
+        };
+    }
+
+    private async Task<APIGatewayProxyResponse> CreateTask(APIGatewayProxyRequest request)
+    {
+        var requestBody = JsonSerializer.Deserialize<Dictionary<string, string>>(request.Body);
+
+        if (!requestBody.TryGetValue("title", out string title))
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest,
+                Body = "{\"message\":\"Missing 'title' in request body.\"}"
+            };
+        }
+
+        var taskItem = new TaskItem
+        {
+            TaskId = Guid.NewGuid().ToString(),
+            Title = title,
+            IsComplete = false
+        };
+
+        await _context.SaveAsync(taskItem);
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.Created,
+            Body = JsonSerializer.Serialize(taskItem),
+            Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" }
+            }
+        };
+    }
+
+    private async Task<APIGatewayProxyResponse> ListTasks()
+    {
+        var scanResult = await _context.ScanAsync<TaskItem>((IEnumerable<ScanCondition>)null).GetRemainingAsync();
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = JsonSerializer.Serialize(scanResult),
+            Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" }
+            }
+        };
     }
 }
